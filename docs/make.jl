55ce0b550fe3b4fdbaff799d3eaffda477e5d2f0
Nathanael Wong <natgeo.wong@outlook.com>
2024-02-29 23:34:44 -0500

    Test update

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,7 @@ makedocs(;
     warnonly = true,
     authors  = "Nathanael Wong",
     repo     = "https://github.com/natgeo-wong/blog",
-    sitename = "Blog",
+    sitename = "The CliNat Blog",
     format   = DocumenterVitepress.MarkdownVitepress(
         repo       = "https://github.com/natgeo-wong/blog.git",
         devurl     = "dev",
