3122583a4f90ac8a234f0130f05fee1fac464535
Nathanael Wong <natgeo.wong@outlook.com>
2024-03-01 01:51:01 -0500

    Restore back to original

--- src/.vitepress/theme/index.ts
+++ src/.vitepress/theme/index.ts
@@ -1,6 +1,19 @@
+// .vitepress/theme/index.ts
+import { h } from 'vue'
+import type { Theme } from 'vitepress'
+import DefaultTheme from 'vitepress/theme'
+
+import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
 import './style.css'
-import Layout from './Layout.vue'
 
 export default {
-  Layout
-}
+  extends: DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+    })
+  },
+  enhanceApp({ app, router, siteData }) {
+    enhanceAppWithTabs(app)
+  }
+} satisfies Theme
